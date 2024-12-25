5c934df3d5e9246b3746b05b9c65692f49dc743e
yasser farag <93351795+yasserfarag22@users.noreply.github.com>
2024-12-25 23:36:48 +0200

    Init With Post

---
+++ app/Livewire/CreatePost.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class CreatePost extends Component
+{
+
+    #[Validate('required|min:5')]
+    public string $title = '';
+
+    #[Validate('required|min:5')]
+    public string $body = '';
+
+    public bool $success= false;
+    public function save(): void
+    {
+        $this->validate();
+
+        Post::create([
+            'title' => $this->title,
+            'body' => $this->body,
+        ]);
+        $this->success = true;
+
+        $this->reset('title', 'body');
+    }
+
+    public function render()
+    {
+        return view('livewire.create-post');
+    }
+
+}
